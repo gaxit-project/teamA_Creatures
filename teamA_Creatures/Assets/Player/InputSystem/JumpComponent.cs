db01d21e3b4a26cb868f87389420e1bea57c7698
辻響 <0131Hibiki-0131@users.noreply.github.com>
2025-01-15 15:09:53 +0900

    2025/01/15 辻 ジャンプまわりの音修正

--- teamA_Creatures/Assets/Player/InputSystem/JumpComponent.cs
+++ teamA_Creatures/Assets/Player/InputSystem/JumpComponent.cs
@@ -7,65 +7,78 @@ public class JumpComponent : MonoBehaviour
     public static JumpComponent Instance;
 
     Animator animator;
-    AudioSource audioSource;
+    AudioSource jumpAudioSource;
+    AudioSource landingAudioSource;
     Rigidbody rb;
-    [SerializeField] private Vector3 localGravity;
+    [SerializeField] private Vector3 localGravity = new Vector3(0, -9.81f, 0);
 
     public float jumpForce = 10f;
     public bool jumpFlag;
     public AudioClip jumpingSound;
     public AudioClip landingSound;
 
+    private bool isLanding = false;
+
     private void Awake()
     {
-        if(Instance == null)
+        if (Instance == null)
         {
             Instance = this;
         }
-        else if(Instance != this)
+        else if (Instance != this)
         {
             Destroy(gameObject);
         }
 
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
-        audioSource = GetComponent<AudioSource>();
+
+        jumpAudioSource = gameObject.AddComponent<AudioSource>();
+        landingAudioSource = gameObject.AddComponent<AudioSource>();
     }
 
     private void Start()
     {
         rb.useGravity = false;
+        rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
         jumpFlag = true;
     }
 
-
     private void FixedUpdate()
     {
-        SetLocalGravity();
+        if (!isLanding) SetLocalGravity();
     }
+
     private void SetLocalGravity()
     {
         rb.AddForce(localGravity, ForceMode.Acceleration);
     }
 
-
     public void JumpVertical(float Jump)
     {
-        if (Jump > 0.7 && jumpFlag)
+        if (Jump > 0.7f && jumpFlag)
         {
-            rb.AddForce(Vector2.up * jumpForce ,ForceMode.Impulse);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             jumpFlag = false;
             animator.SetTrigger("Jump");
-            audioSource.PlayOneShot(jumpingSound);
+            jumpAudioSource.PlayOneShot(jumpingSound);
         }
     }
 
-    public void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Ground")
+        if (collision.gameObject.CompareTag("Ground") && !isLanding)
         {
+            isLanding = true;
             jumpFlag = true;
-            audioSource.PlayOneShot(landingSound);
+            landingAudioSource.PlayOneShot(landingSound);
+            StartCoroutine(ResetLandingFlag());
         }
+    }
+
+    private IEnumerator ResetLandingFlag()
+    {
+        yield return new WaitForSeconds(0.1f);
+        isLanding = false;
     }
 }
