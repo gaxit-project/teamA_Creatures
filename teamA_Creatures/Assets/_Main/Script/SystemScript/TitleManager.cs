d9ed8611fc4b4de3322193c7c0e11aac81604e98
辻響 <0131Hibiki-0131@users.noreply.github.com>
2025-02-19 17:13:57 +0900

    2025/02/19 辻 文字色変更

--- teamA_Creatures/Assets/_Main/Script/SystemScript/TitleManager.cs
+++ teamA_Creatures/Assets/_Main/Script/SystemScript/TitleManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class TitleManager : MonoBehaviour
 {
@@ -11,17 +12,55 @@ public class TitleManager : MonoBehaviour
     public Button endButton;
     public Setting settingScript;
 
+    private Color defaultColor = Color.black; // 非選択時の色
+    private Color selectedColor = Color.white; // 選択時の色
+
     void Start()
     {
         startButton.onClick.AddListener(StartGame);
         settingButton.onClick.AddListener(OpenSetting);
         endButton.onClick.AddListener(QuitGame);
+
+        // 各ボタンにイベントを設定
+        AddButtonSelectionEffect(startButton);
+        AddButtonSelectionEffect(settingButton);
+        AddButtonSelectionEffect(endButton);
+
         startButton.Select();
+    }
+
+    void AddButtonSelectionEffect(Button button)
+    {
+        EventTrigger trigger = button.gameObject.AddComponent<EventTrigger>();
+
+        // 選択時の処理
+        EventTrigger.Entry onSelectEntry = new EventTrigger.Entry
+        {
+            eventID = EventTriggerType.Select
+        };
+        onSelectEntry.callback.AddListener((data) => ChangeTextColor(button, selectedColor));
+        trigger.triggers.Add(onSelectEntry);
+
+        // 非選択時の処理
+        EventTrigger.Entry onDeselectEntry = new EventTrigger.Entry
+        {
+            eventID = EventTriggerType.Deselect
+        };
+        onDeselectEntry.callback.AddListener((data) => ChangeTextColor(button, defaultColor));
+        trigger.triggers.Add(onDeselectEntry);
+    }
+
+    void ChangeTextColor(Button button, Color color)
+    {
+        Text buttonText = button.GetComponentInChildren<Text>();
+        if (buttonText != null)
+        {
+            buttonText.color = color;
+        }
     }
 
     public void StartGame()
     {
-        // まずロードシーンへ遷移
         SceneManager.LoadScene("Loading");
     }
 
