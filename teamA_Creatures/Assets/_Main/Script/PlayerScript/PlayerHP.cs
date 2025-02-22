11d0dff6ff62da18298a3717732caebaefef63ec
LUCA <luklar23.oit.game@gmail.com>
2025-02-22 11:24:14 +0900

    ビーストモード時の攻撃力を上げた

--- teamA_Creatures/Assets/_Main/Script/PlayerScript/PlayerHP.cs
+++ teamA_Creatures/Assets/_Main/Script/PlayerScript/PlayerHP.cs
@@ -10,6 +10,8 @@ public class PlayerHP : MonoBehaviour
     public EnemyEffect EE;
     public PlayerEffect PE;
 
+    public static float BeastModeHP = 0f;
+
     public void Awake()
     {
         if (Instance == null)
@@ -32,6 +34,7 @@ public class PlayerHP : MonoBehaviour
     public bool muteki;
     void Start()
     {
+        BeastModeHP = 0f;
         playerMaxHP = 100;
         playerHP = playerMaxHP;
         animator = GetComponent<Animator>();
@@ -79,7 +82,7 @@ public class PlayerHP : MonoBehaviour
 
                 if (other.gameObject.tag == "EnemyPunchAttack")
                 {
-                    playerHP = playerHP - 10;
+                    playerHP = playerHP - (10 * BeastModeHP);
                     EE.PunchEffect();
                     PE.DamageEffect();
                     animator.SetTrigger("falter");
@@ -97,7 +100,7 @@ public class PlayerHP : MonoBehaviour
 
                 if (other.gameObject.tag == "EnemyChainAttack")
                 {
-                    playerHP = playerHP - 10;
+                    playerHP = playerHP - (10 * BeastModeHP);
                     animator.SetTrigger("falter");
                     HitNow = true;
                     muteki = true;
@@ -110,7 +113,7 @@ public class PlayerHP : MonoBehaviour
 
                 if (other.gameObject.tag == "EnemyTackleAttack")
                 {
-                    playerHP = playerHP - 20;
+                    playerHP = playerHP - (20 * BeastModeHP);
                     PE.TackleDamageEffect();
                     animator.SetTrigger("falter");
                     HitNow = true;
@@ -125,7 +128,7 @@ public class PlayerHP : MonoBehaviour
 
                 if (other.gameObject.tag == "EnemySmashAttack")
                 {
-                    playerHP = playerHP - 30;
+                    playerHP = playerHP - (30 * BeastModeHP);
                     EE.SmashEffect();
                     PE.DamageEffect();
                     animator.SetTrigger("EnemyTackleHit");
